17a296a186f4a8caaf00e510380a767f106cb685
Man <nguyenman025@gmail.com>
2025-03-20 23:58:14 -0400

    finished functional menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,38 +3,80 @@ import '../styles/menu.css';
 const items = [
     {name: 'Original Milk Tea', price: {sm: 4.85, lg: 5.75}, category: 'Classic Milk Tea'},
     {name: 'Brown Sugar Milk Tea', price: {sm: 4.85, lg: 5.75}, category: 'Classic Milk Tea'},
-    {name: 'Mango Milk Tea', price: {sm: 4.85, lg: 5.75}, category: 'Classic Milk Tea'}
+    {name: 'Mango Milk Tea', price: {sm: 4.85, lg: 5.75}, category: 'Classic Milk Tea'},
+    {name: 'Taro Milk Tea', price: {sm: 4.85, lg: 5.75}, category: 'Classic Milk Tea'},
+    {name: 'Golden Grapefruit', price: {sm: null, lg: 5.45}, category: 'Fresh Fruit Tea'},
+    {name: 'Passion Fruit Green Tea', price: {sm: null, lg: 5.75}, category: 'Fresh Fruit Tea'},
+    {name: 'Mango Lemon Green Tea', price: {sm: null, lg: 5.75}, category: 'Fresh Fruit Tea'},
+    {name: 'Peach Lemon Green Tea', price: {sm: null, lg: 5.75}, category: 'Fresh Fruit Tea'},
+    {name: 'Lychee Black Milk Tea', price: {sm: null, lg: 5.75}, category: 'Special Milk Tea'},
+    {name: 'Peach Oolong Milk Tea', price: {sm: null, lg: 5.75}, category: 'Special Milk Tea'},
+    {name: 'Rose Black Milk Tea', price: {sm: null, lg: 5.75}, category: 'Special Milk Tea'},
+    {name: 'Ruby Black Tea', price: {sm: null, lg: 5.45}, category: 'Fresh Brewed Tea'},
+    {name: 'Rose Black Tea', price: {sm: null, lg: 5.45}, category: 'Fresh Brewed Tea'},
 ]
 const Menu = (content) => {
+    // Create menu
     const menu = document.createElement('div');
     menu.classList.add('menu');
-
     const createCard = (card) => {
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('menu-card');
-        cardDiv.innerHTML = `
-            <h3>${card.name}</h3>
-            <div class="prices">
-                <span>Small: ${card.price.sm}</span>
-                <span>Large: ${card.price.lg}</span>
-            </div>
-        `;
+        const cardName = document.createElement('h3');
+        cardName.textContent = card.name;
+        cardDiv.appendChild(cardName);
+        const cardPrice = document.createElement('div');
+        cardPrice.classList.add('prices');
+        if (card.price.sm !== null){
+            const sm = document.createElement('span');
+            sm.textContent = `Small: ${card.price.sm}`;
+            cardPrice.appendChild(sm);
+        }
+        if (card.price.lg !== null){
+            const lg = document.createElement('span');
+            lg.textContent = `Large: ${card.price.lg}`;
+            cardPrice.appendChild(lg);
+        }
+        cardDiv.appendChild(cardPrice);
         return cardDiv;
     }
-    const createSection = (() => {
+    const createSection = (category) => {
         const section = document.createElement('section');
         section.classList.add('menu-section');
+        section.classList.add(`${category.toLowerCase().split(' ').join('-')}`);
         const collaspableBtn = document.createElement('button');
-        collaspableBtn.textContent = 'Open Collapsible';
+        collaspableBtn.innerHTML = `<h2>${category}</h2>`;
+        const collapseIcon = document.createElement('span');
+        collapseIcon.textContent = 'arrow_drop_down';
+        collapseIcon.classList.add('material-symbols-outlined');
+        collapseIcon.classList.add('collapse-icon');
+        collaspableBtn.appendChild(collapseIcon);
         collaspableBtn.type = 'button';
         collaspableBtn.classList.add('collapsible');
         section.appendChild(collaspableBtn);
-        items.forEach((item) => section.appendChild(createCard(item)));
+        const cardsContainer = document.createElement('div');
+        cardsContainer.classList.add('cards-container');
+        cardsContainer.style.height = 'auto';
+        items.filter((item) => item.category == category).forEach((item) => cardsContainer.appendChild(createCard(item)));
+        section.appendChild(cardsContainer);
         collaspableBtn.addEventListener('click', () => {
-            [...section.children].slice(1).forEach((item) => item.style.display == 'none'? item.style.display = 'flex':item.style.display = 'none');
-        });
+            // when collapse is clicked we rotate icon and collapse menu using height
+            collapseIcon.classList.toggle('rotate');
+            if (!cardsContainer.classList.contains('collapsed')) {
+                cardsContainer.style.height = `${cardsContainer.scrollHeight}px`; // Ensures the height matches content height before collapsing
+                setTimeout(() => (cardsContainer.style.height = '0'), 0);
+            } else {
+            cardsContainer.style.height = `${cardsContainer.scrollHeight}px`;
+            setTimeout(() => (cardsContainer.style.height = 'auto'), 300); // After transition, set to auto for dynamic content
+            }
+            cardsContainer.classList.toggle('collapsed');
+            });
         menu.appendChild(section);
-    })();
+    };
+
+    // create sections for each category
+    const categories = new Set(items.map((item) => item.category));
+    categories.forEach((category) => createSection(category));
     content.appendChild(menu);
 };
 
